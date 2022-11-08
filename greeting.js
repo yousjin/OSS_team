5d43c08e615e369de6717f0b908cf197710566c3
Seheyon <104759146+Seheyon@users.noreply.github.com>
2022-11-08 20:04:02 +0900

    test

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
 const greeting = function (rtm, channel) {
   console.log('인사를 합니다.');
-  rtm.sendMessage('hello!', channel);
+  rtm.sendMessage('hello!!!', channel);
 };
 
 module.exports = greeting;
