645830bdf39489724d19b9d4d412a2bf5f52a244
Cheon <cheonsa30@naver.com>
2022-11-17 23:44:07 +0900

    greeting.js add variation

--- greeting.js
+++ greeting.js
@@ -1,6 +1,30 @@
 const greeting = function (rtm, channel) {
   console.log('인사를 합니다.');
-  rtm.sendMessage('hello!', channel);
+
+  const randomNum = Math.random() * 3;
+  const randomNumFloor = Math.floor(randomNum);
+
+  switch (randomNumFloor) {
+    case 0:
+      rtm.sendMessage('안녕하세요!', channel);
+      return 'success';
+    case 1:
+      rtm.sendMessage('좋은 하루입니다!', channel);
+      return 'success';
+    case 2:
+      rtm.sendMessage('반가워요!', channel);
+      return 'success';
+    default:
+      return 'fail';
+  }
+
+  /* try {
+    rtm.sendMessage('hello', channel);
+    return Promise.resolve('success');
+  } catch (error) {
+    console.log('error', error.data);
+    return Promise.resolve('error');
+  } */
 };
 
 module.exports = greeting;
