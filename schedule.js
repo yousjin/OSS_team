b2999ffd9ef7f4cf7a870cef08b8354a0ac5c970
yousjin <syg2720@hanmail.net>
2022-11-21 23:11:07 +0900

    두번쨰 수정

--- schedule.js
+++ schedule.js
@@ -1,7 +1,7 @@
 const schedule = function (rtm, text, channel) {
 
   const regex = /([1-9]|1[012])\/([1-9]|[123][0-9])/;
-  var arr = ["8/4", "8/22", "9/1", "9/2", "9/3", "9/4", "9/6", "9/7", "10/15", "10/19", "10/20", "10/21", "10/27", "11/3", "11/24", "12/21", "12/23"]
+  var arr = ["8/4", "8/22", "9/1", "9/2", "9/3", "9/4", "9/5", "9/6", "9/7", "10/15", "10/19", "10/20", "10/21", "10/27", "11/3", "11/24", "12/21", "12/23"]
   var arrIndex = [[0], [1], [2,3], [3], [3], [3,4], [3], [3], [3], [5], [6] ,[6], [6], [7], [8], [9], [10], [11]]
 
   const fs = require('fs')
@@ -20,10 +20,12 @@ const schedule = function (rtm, text, channel) {
     if (index == -1){
       rtm.sendMessage('해당 날짜에 대한 일정이 없습니다', channel);
     }else{
-      for (var i in arrIndex[index]){
-        var contents = haksa[i].toString().split(' : ')
-        rtm.sendMessage(contents[1], channel)
+      for (var i=0; i< arrIndex[index].length; i++){
+        var contents = haksa[arrIndex[index][i]].toString().split(' : ');
+        rtm.sendMessage(contents[1], channel);
       }
+
+
     }
       
   }else{    //형식에 안맞는 경우
