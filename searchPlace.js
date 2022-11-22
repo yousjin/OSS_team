0f22e9c6cb0046e34c6626b0ef72bd1e2d5d73ea
Cheon <cheonsa30@naver.com>
2022-11-22 10:11:22 +0900

    combine all function

--- searchPlace.js
+++ searchPlace.js
@@ -1,29 +1,30 @@
+/* eslint-disable quotes */
 const searchPlace = function (rtm, channel, str, num) {
   try {
-    var De_place;
     const DeptKor = '건축공학부,기계공학부,도시공학부,전자공학부,컴퓨터공학부,화학공학부,회계학부,국제무역학부,국어국문학부,문헌정보학부';
     const DeptKorArr = DeptKor.toString().split(',');
 
+    // eslint-disable-next-line global-require
     const fs = require('fs');
 
     const article = fs.readFileSync("dept.txt");
     const textSplitArr = article.toString().split("\n");
 
-    var n = num;
-    if(n == -1) {
+    let n = num;
+    if (n === -1) {
       n = DeptKorArr.indexOf(str);
     }
 
-    if (n == -1) {
+    if (n === -1) {
       rtm.sendMessage("i m alive", channel);
     }
     console.log("학과 있음");
 
-    DeptArr = textSplitArr[n].toString().split('-');
-    De_place = DeptArr[1];
+    const DeptArr = textSplitArr[n].toString().split('-');
+    const DePlace = DeptArr[1];
 
-    rtm.sendMessage(De_place, channel);
-  } catch(error) {
+    rtm.sendMessage(DePlace, channel);
+  } catch (error) {
     console.log("error!", error.data);
   }
 };
