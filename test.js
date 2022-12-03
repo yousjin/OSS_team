094ffab518d67857e6ac98b6f7110bc7c93230ac
Seheyon <104759146+Seheyon@users.noreply.github.com>
2022-12-03 16:09:05 +0900

    #13 feat: feature 4 eng test add

--- test.js
+++ test.js
@@ -25,13 +25,12 @@ const rtm = new RTMClient(token);
 const assert = require('assert');
 const greeting = require('./greeting');
 
-describe('테스트 시작', () => {
-  it('greeting 함수 테스트', (done) => {
-    assert.equal(greeting(rtm, channel), 'success');
-    assert.equal(greeting(rtm, channel), 'success');
-    assert.equal(greeting(rtm, channel), 'success');
-    assert.equal(greeting(rtm, channel), 'success');
-    assert.equal(greeting(rtm, channel), 'success');
-    done();
-  });
+describe('searchPlace 함수 테스트', () => {
+    it('장소 반환', () => {
+      assert.equal(searchPlace(rtm, channel, 'architect ural En  gineering'), ' College of Engineering Building 1, 132');
+      assert.equal(searchPlace(rtm, channel, 'mec hanical enginee ring'), ' College of Engineering Building 4, 212');
+      assert.equal(searchPlace(rtm, channel, 'URBANENGINEERING'), ' College of Engineering Building 9, 917');
+      assert.equal(searchPlace(rtm, channel, 'ElectronicEngineering'), ' College of Engineering Building 7, 224');
+      assert.equal(searchPlace(rtm, channel, 'Computer Science and Engineering'), ' College of Engineering Building 7, 224');
+    });
 });
