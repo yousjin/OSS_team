3225185e75c26992d22fb329539222ee87a729d5
Seheyon <104759146+Seheyon@users.noreply.github.com>
2022-12-03 18:03:39 +0900

    #19 feat: feature4 - 3 levenshtein distance

--- searchPlace.js
+++ searchPlace.js
@@ -1,5 +1,6 @@
 /* eslint-disable quotes */
 const searchPlace = function (rtm, channel, str) {
+  const levenshtein = require('js-levenshtein');
   try {
     // eslint-disable-next-line global-require
     const fs = require('fs');
@@ -8,6 +9,7 @@ const searchPlace = function (rtm, channel, str) {
     const textSplitArr = article.toString().split("\n");
 
     let n = -1;
+    str = str + ' ';
     str = str.replace(/(\s*)/g, "");
 
     for(i in textSplitArr) {
@@ -15,13 +17,18 @@ const searchPlace = function (rtm, channel, str) {
       var text = DeptArr[0].replace(/(\s*)/g, "");
 
       if(text.toUpperCase() == str.toUpperCase()) {
-          n = i;
-          rtm.sendMessage(DeptArr[1], channel);
-          console.log("학과 있음");
-          return DeptArr[1];
-        }
-    }
+        n = i;
+        rtm.sendMessage(DeptArr[1], channel);
+        console.log("학과 있음");
+        return 'success';
+      }
 
+      if(levenshtein(text, str) < 4) {
+        rtm.sendMessage(DeptArr[0] + "를 원하셨나요? 해당 위치는 "  + DeptArr[1] + " 입니다.", channel);
+        console.log("학과 있음");
+        return 'success';
+      }
+    }
     if (n === -1) {
       rtm.sendMessage("i m alive", channel);
     }
