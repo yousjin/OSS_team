e80fe5636b128ef87c4e051bdbd5df9ba1b88611
Seheyon <104759146+Seheyon@users.noreply.github.com>
2022-12-03 16:20:18 +0900

    error: 띄어쓰기 수정

--- test.js
+++ test.js
@@ -27,7 +27,7 @@ const searchPlace = require('./searchPlace');
 
 describe('searchPlace 함수 테스트', () => {
     it('장소 반환', () => {
-      assert.equal(searchPlace(rtm, channel, 'architect ural En  gineering'), ' College of Engineering Building 1, 132');
+      assert.equal(searchPlace(rtm, channel, 'architect ural En  gineering'), '  College of Engineering Building 1, 132');
       assert.equal(searchPlace(rtm, channel, 'mec hanical enginee ring'), ' College of Engineering Building 4, 212');
       assert.equal(searchPlace(rtm, channel, 'URBANENGINEERING'), ' College of Engineering Building 9, 917');
       assert.equal(searchPlace(rtm, channel, 'ElectronicEngineering'), ' College of Engineering Building 7, 224');
